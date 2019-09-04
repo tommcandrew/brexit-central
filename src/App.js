f68fcb8bab8073b87bcb64ed34c35a072b5be0ce
Thomas McAndrew <tommcandrew@hotmail.com>
2019-09-04 17:09:30 +0100

    created countdown comp, added css and added flag png in new assets folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css'
+import Countdown from './Components/Countdown/Countdown'
 
 function App () {
   return (
     <div className='App'>
+       <Countdown />
       <h1>Brexit Planner</h1>
     </div>
   )

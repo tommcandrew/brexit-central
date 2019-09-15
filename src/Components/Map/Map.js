46400a0a0e5a22a8b632bd962389d36c5279e4e3
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-09-15 17:06:55 +0100

    add event handler to dropdown

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -7,17 +7,44 @@ class Map extends React.Component {
     this.state = {
     outBoundAirports: [],
     inBoundsAirports: [],
-    sessionKey: null
+    sessionKey: null,
+    selectValue: ""
     };
     this.createSession = this.createSession.bind(this);
     this.handleState = this.handleState.bind(this);
     this.getSessionResults = this.getSessionResults.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     this.createSession();
   }
   
+  createSession() {
+    var data = "inboundDate=2019-10-31&cabinClass=business&children=0&infants=0&country=UK&currency=GBP&locale=en-UK&originPlace=LOND-sky&destinationPlace=PARI-sky&outboundDate=2019-10-31&adults=1";
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+  
+    xhr.addEventListener("readystatechange", () => {
+      if (this.readyState === this.DONE) {
+       console.log(this.responseText);
+       this.setState({sessionKey:  xhr.getResponseHeader("location")}, this.handleState());
+       console.log("state: " + this.state.sessionKey);
+      }
+    });
+
+    const url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0";
+    xhr.open("POST", url);
+    xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "7950dacff7mshd54ef549a82264bp1e0133jsne29e4dd7aa24");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+  
+  
+    xhr.send(data);
+    console.log("state: " + this.state.sessionKey);
+    }
+
   handleState(){
     console.log("stateHANDLE: " + this.state.sessionKey);
     if(this.state.sessionKey !== null){
@@ -51,46 +78,22 @@ class Map extends React.Component {
     xhr.send(data);
   }
 
-  createSession() {
-    var data = "inboundDate=2019-10-31&cabinClass=business&children=0&infants=0&country=UK&currency=GBP&locale=en-UK&originPlace=LOND-sky&destinationPlace=PARI-sky&outboundDate=2019-10-31&adults=1";
-
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-  
-    xhr.addEventListener("readystatechange", () => {
-      if (this.readyState === this.DONE) {
-       console.log(this.responseText);
-       this.setState({sessionKey:  xhr.getResponseHeader("location")}, this.handleState());
-       console.log("state: " + this.state.sessionKey);
-      }
+  handleChange(e){
+    this.setState({ selectValue: e.target.value }, () => {
+      console.log("selected value: " + this.state.selectValue);
     });
+  }
 
-    const url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0";
-    xhr.open("POST", url);
-    xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "7950dacff7mshd54ef549a82264bp1e0133jsne29e4dd7aa24");
-    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-  
-  
-    xhr.send(data);
-    console.log("state: " + this.state.sessionKey);
-  // const sessionKey =  this.state.sessionKey.replace(url, "");
-    
-   // this.getSessionResult(sessionKey);
-    }
-
- /* getSessionResult(sessionKey) {
-   
-   }*/
 render() {
       return (
         <div>
-        <select>
-            <option value="london-city">London City</option>
-            <option value="london-gatwick">London Gatwick</option>
-            <option value="london-heathrow">London Heathrow</option>
-            <option value="london-luton">London Luton</option>
-            <option value="london-stanstead">London Stanstead</option>
+        <select id="dropdown"  onChange={this.handleChange}>
+            <option value="Select" defaultValue>Select</option>
+            <option value="LCY">London City</option>
+            <option value="LGW">London Gatwick</option>
+            <option value="LHR">London Heathrow</option>
+            <option value="LTN">London Luton</option>
+            <option value="STN">London Stanstead</option>
         </select>
         </div>
       );

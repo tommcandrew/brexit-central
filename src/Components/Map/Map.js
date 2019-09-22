270a8ce9a693a420e9f61f208ec76ff8e4af8e16
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-09-22 15:59:42 +0100

    change function to fetch

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -5,54 +5,74 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    selectValue: ""
+      error: null,
+      isLoaded: false,
+      quotes: [],
+      selectValue: ""
     };
- 
-    this.handleChange = this.handleChange.bind(this);
     this.getQuotes = this.getQuotes.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   getQuotes() {
-    var chosenOriginPlace = this.state.selectValue;
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-UK/" + chosenOriginPlace + "-sky/anywhere/2019-10-31?inboundpartialdate=2019-12-01", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        "x-rapidapi-key": "7950dacff7mshd54ef549a82264bp1e0133jsne29e4dd7aa24"
-      }
-    })
-    .then(response => {
-    var myArr =  response.json();
-    console.log(myArr);
-    return myArr
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    let chosenOriginPlace = this.state.selectValue;
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-UK/" + chosenOriginPlace + "-sky/anywhere/2019-10-31?inboundpartialdate=2019-12-01")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            quotes: result.Quotes
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   handleChange(e){
     this.setState({ selectValue: e.target.value }, () => {
       console.log("selected value: " + this.state.selectValue);
-     // this.createSessin();
      this.getQuotes();
     });
   }
 
-render() {
+  render() {
+    const { error, isLoaded, quotes } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+       return <select id="dropdown"  onChange={this.handleChange}>
+          <option value="Select" defaultValue>Select</option>
+          <option value="LCY">London City</option>
+          <option value="LGW">London Gatwick</option>
+          <option value="LHR">London Heathrow</option>
+          <option value="LTN">London Luton</option>
+          <option value="STN">London Stanstead</option>
+      </select>
+    } else {
       return (
-        <div>
-        <select id="dropdown"  onChange={this.handleChange}>
-            <option value="Select" defaultValue>Select</option>
-            <option value="LCY">London City</option>
-            <option value="LGW">London Gatwick</option>
-            <option value="LHR">London Heathrow</option>
-            <option value="LTN">London Luton</option>
-            <option value="STN">London Stanstead</option>
-        </select>
-        </div>
+        <ul>
+          {quotes.map(quote => (
+            <li key={quote.QuoteId}>
+              {quote.QuoteId} {quote.MinPrice}
+            </li>
+          ))}
+        </ul>
       );
     }
-} 
+  }
+}
+
+
+
+
+
+
+
 
 export default Map

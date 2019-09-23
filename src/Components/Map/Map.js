5dd3f04becd94a280eaed61321643f50cb0ce82b
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-09-23 17:03:39 +0100

    fix flights sorting

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -13,7 +13,7 @@ class Map extends React.Component {
     this.getQuotes = this.getQuotes.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.getResults = this.getResults.bind(this);
-    this.sortBubble = this.sortBubble.bind(this);
+    this.getCheapest = this.getCheapest.bind(this);
   }
 
   getQuotes() {
@@ -43,8 +43,8 @@ class Map extends React.Component {
       )
   }
 
-  //To get only 20 the cheapest filghts
-  getResults() {
+ //get only the direct flights which are under £100
+ getResults() {
     let quotes = this.state.quotes;
     let result = [];
     let price;
@@ -52,58 +52,30 @@ class Map extends React.Component {
     let item = [];
 
     for(let i = 0; i < quotes.length; i++){
+      if(quotes[i].Direct === true && quotes[i].MinPrice < 100){
         place = quotes[i].OutboundLeg.DestinationId;
         price = quotes[i].MinPrice;
-        item = [place, price];
-        result.push(item);
-    }
-    console.log(result);
-    
-    this.sortBubble(result);
-  }
-
-  
-  //bubble sort
-  sortBubble(result) {
-      let len = result.length;
-      let swapped;
-      do {
-          swapped = false;
-          for (let i = 0; i < len - 1; i++) {
-              if (result[i][1] > result[i + 1][1]) {
-                 console.log(result[i][1]);
-                 console.log(result[i + 1][1]);
-                  let tmp = result[i];
-                  result[i] = result[i + 1];
-                  result[i + 1] = tmp;
-                  swapped = true;
-              }
-          }
-      } while (swapped);
-      console.log(result);
-      this.setState({ quotes: result.slice(0,20)}, () => console.log(this.state.quotes));
-  }
-
- //To get only the direct flights 
- /* getResults() {
-    let quotes = this.state.quotes;
-    let result = [];
-    let price;
-    let place;
-    let item = [];
-
-    for(let i = 0; i < quotes.length; i++){
-      if(quotes[i].Direct === true){
-        place = "Place: " + quotes[i].OutboundLeg.DestinationId;
-        price = "Price: " + quotes[i].MinPrice;
         item = [place, price];
         result.push(item);
       }
     }
     console.log("Result: " + result);
     console.log(result.length);
-     this.setState({ quotes: result, () => console.log(this.state.quotes));
-  }*/
+    if(result.length < 20) {
+      this.setState({ quotes: result}, () => console.log(this.state.quotes));
+    } else {
+      this.getCheapest(result);
+    }
+  }
+
+  getCheapest(result) {
+    const sorted = result.sort(function(a , b) {
+      return a[1] - b[1];
+  });;
+    const cheapest = sorted.slice(0,20);
+    console.log(cheapest.length);
+    this.setState({ quotes: cheapest }, () => console.log("cheapest: " + this.state.quotes));
+  }
 
   handleChange(e){
     this.setState({ selectValue: e.target.value }, () => {
@@ -114,13 +86,28 @@ class Map extends React.Component {
 
   render() {
     return(
-      <select id="dropdown"  onChange={this.handleChange}>
+     <select id="dropdown"  onChange={this.handleChange}>
           <option value="Select" defaultValue>Select</option>
-          <option value="LCY">London City</option>
-          <option value="LGW">London Gatwick</option>
-          <option value="LHR">London Heathrow</option>
-          <option value="LTN">London Luton</option>
-          <option value="STN">London Stanstead</option>
+          <option value="ABZ">Aberdeen</option>
+          <option value="BFS">Belfast</option>
+          <option value="BHX">Birmingham</option>
+          <option value="BOH">Bournemouth</option>
+          <option value="BRS">Bristol</option>
+          <option value="CWL">Cardiff</option>
+          <option value="DSA">Doncaster Sheffield</option>
+          <option value="EDI">Edinburgh</option>
+          <option value="EXT">Exeter</option>
+          <option value="GLA">Glasgow</option>
+          <option value="GCI">Guernsey</option>
+          <option value="INV">Inverness</option>
+          <option value="JER">Jersey</option>
+          <option value="LBA">Leeds Bradford</option>
+          <option value="LPL">Liverpool</option>
+          <option value="LOND">London</option>
+          <option value="MAN">Manchester</option>
+          <option value="NCL">Newcastle</option>
+          <option value="NWI">Norwich</option>
+          <option value="SOU">Southampton</option>
       </select>
     )
   }

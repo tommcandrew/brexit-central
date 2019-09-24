3d9ca5c0852ad55f99bb24b57ea1d2e775ed5f66
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-09-24 19:06:32 +0100

    return country airport price

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -6,14 +6,12 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
       quotes: [],
       places: [],
       selectValue: ""
     };
-    this.getQuotes = this.getQuotes.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.getQuotesAndPlaces = this.getQuotesAndPlaces.bind(this);
     this.getResults = this.getResults.bind(this);
     this.getCheapest = this.getCheapest.bind(this);
   }
@@ -21,11 +19,11 @@ class Map extends React.Component {
   handleChange(e){
     this.setState({ selectValue: e.target.value }, () => {
       console.log("Selected: " + this.state.selectValue);
-      this.getQuotes();
+      this.getQuotesAndPlaces();
     });
   }
 
-  getQuotes() {
+  getQuotesAndPlaces() {
     let chosenOriginPlace = this.state.selectValue;
 
     axios.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-UK/" + chosenOriginPlace + "-sky/anywhere/2019-10-31?inboundpartialdate=2019-12-01", {
@@ -42,74 +40,75 @@ class Map extends React.Component {
     })
   }
 
-  //get only the direct flights which are under £100
   getResults() {
     const quotes = this.state.quotes;
     const places = this.state.places;
-    const result = [];
-    let minPrice;
-    let destinationID;
-    let item= [];
-    let placesQuotes = [];
-    let final = [];
+    const quotesDestionationIdPrice = [];
+    const quotesDestinationId = [];
+    const placesIdCountryAirport = [];
+    const placesId = [];
+    const results = [];
+    
+    //get only direct flights which are under £100
 
     for(let i = 0; i < quotes.length; i++){
       if(quotes[i].Direct === true && quotes[i].MinPrice < 100){
-        destinationID = quotes[i].OutboundLeg.DestinationId;
-        placesQuotes.push(destinationID);
-        minPrice = quotes[i].MinPrice;
-        item = [destinationID, minPrice];
-        result.push(item);
+        quotesDestinationId.push(quotes[i].OutboundLeg.DestinationId);
+        quotesDestionationIdPrice.push([quotes[i].OutboundLeg.DestinationId, quotes[i].MinPrice]);
       }
     }
 
-    let placesId = [];
-    console.log(placesQuotes);
-    // map country id and name into an array
-    const placesIdAndName = [];
+    //add place ID, country name and airport name to separate array
+
       for(let i = 0; i < places.length; i++){
-        placesIdAndName.push([places[i].PlaceId, places[i].CountryName]);
+        placesIdCountryAirport.push([places[i].PlaceId, places[i].CountryName, places[i].Name]);
         placesId.push(places[i].PlaceId);
       }
     
-    console.log(placesIdAndName);
+    console.log(placesIdCountryAirport);
 
-    if(result.length < 20) {
-       //filter Places respone to get rid off the duplicates:
-      let current;
-      for(let i = 0; i < result.length; i++){
-      current = placesId.indexOf(result[i][0]);
-      console.log("Current " +  current);
-      if(current !== -1){
-        final.push([placesIdAndName[current][1], result[i][1]]);
-      }
+    if(quotesDestionationIdPrice.length < 20) {
+      
+       //filter Places respone to include only destination ids that correspond to the Quotes destination ids
+
+      let index;
+
+      for(let i = 0; i < quotesDestionationIdPrice.length; i++){
+        index = placesId.indexOf(quotesDestionationIdPrice[i][0]);
+        if(index !== -1){
+          results.push([placesIdCountryAirport[index][1], placesIdCountryAirport[index][2], quotesDestionationIdPrice[i][1]]);
+        }
     }
-    console.log(result.length);
-    console.log(final.length);
-      this.setState({ quotes: final}, () => console.log("[Country, Price] " + this.state.quotes));
+
+    console.log(quotesDestionationIdPrice.length);
+    console.log(results.length);
+
+      this.setState({ quotes: results }, () => console.log("[Country, Airport, Price] " + this.state.quotes));
     } else {
-      this.getCheapest(result,placesId, placesIdAndName);
+      this.getCheapest(quotesDestionationIdPrice, placesId, placesIdCountryAirport);
     }
   }
 
-  getCheapest(result,placesId, placesIdAndName) {
-    let final = [];
-    const sorted = result.sort(function(a , b) {
+  getCheapest(quotesDestionationIdPrice,placesId, placesIdCountryAirport) {
+    const results = [];
+    const sorted = quotesDestionationIdPrice.sort(function(a , b) {
       return a[1] - b[1];
-  });;
+     });;
     const cheapest = sorted.slice(0,20);
-    //filter Places respone to get rid off the duplicates:
-    let current;
+    
+     //filter Places respone to include only destination ids that correspond to the Quotes destination ids
+
+    let index;
+
     for(let i = 0; i < cheapest.length; i++){
-    current = placesId.indexOf(result[i][0]);
-    console.log("Current " +  current);
-    if(current !== -1){
-      final.push([placesIdAndName[current][1], cheapest[i][1]]);
+    index = placesId.indexOf(quotesDestionationIdPrice[i][0]);
+    if(index !== -1){
+      results.push([placesIdCountryAirport[index][1], placesIdCountryAirport[index][2], cheapest[i][1]]);
     }
   }
   console.log(cheapest.length);
-  console.log(final.length);
-    this.setState({ quotes: final }, () => console.log("[Country, Price] - Cheapest " + this.state.quotes));
+  console.log(results.length);
+    this.setState({ quotes: results }, () => console.log("[Country, Airport, Price] - (20 Cheapest) " + this.state.quotes));
   }
 
   render() {

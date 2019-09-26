ff23c2b2b6f07299df2ad170716bf78880f4ea26
marco981 <marco98x2@gmail.com>
2019-09-26 20:38:02 +0100

    cleaning up pt.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
 
+import Map from './Components/Map'
+
 function App () {
   return (
     <div className='App'>
       <h1>Brexit Planner</h1>
+      <Map />
     </div>
   )
 }

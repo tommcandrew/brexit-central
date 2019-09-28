af7f68cd3e7bad6a6ccf81fce84cb6b5c870ac26
marco981 <marco98x2@gmail.com>
2019-09-28 16:05:49 +0100

    change name of GoogleMap in Map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,135 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+import Marker from '../Map/Marker/Marker'
+// import axios from 'axios'
 import './Map.css'
-import axios from 'axios';
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quotes: [],
-      places: [],
-      selectValue: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.getQuotesAndPlaces = this.getQuotesAndPlaces.bind(this);
-    this.getResults = this.getResults.bind(this);
-    this.getCheapest = this.getCheapest.bind(this);
+class Map extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+    this.ref = null
   }
 
-  handleChange(e){
-    this.setState({ selectValue: e.target.value }, () => {
-      console.log("Selected: " + this.state.selectValue);
-      this.getQuotesAndPlaces();
-    });
-  }
-
-  getQuotesAndPlaces() {
-    let chosenOriginPlace = this.state.selectValue;
-
-    axios.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-UK/" + chosenOriginPlace + "-sky/anywhere/2019-10-31?inboundpartialdate=2019-12-01", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        "x-rapidapi-key": "7950dacff7mshd54ef549a82264bp1e0133jsne29e4dd7aa24"
-      }
-    })
-    .then(res => {
-      this.setState({ quotes: res.data.Quotes });
-      this.setState({ places: res.data.Places });
-      this.getResults();
-    })
-  }
-
-  getResults() {
-    const quotes = this.state.quotes;
-    const places = this.state.places;
-    const quotesDestionationIdPrice = [];
-    const quotesDestinationId = [];
-    const placesIdCountryAirport = [];
-    const placesId = [];
-    const results = [];
-    
-    //get only direct flights which are under £100
-
-    for(let i = 0; i < quotes.length; i++){
-      if(quotes[i].Direct === true && quotes[i].MinPrice < 100){
-        quotesDestinationId.push(quotes[i].OutboundLeg.DestinationId);
-        quotesDestionationIdPrice.push([quotes[i].OutboundLeg.DestinationId, quotes[i].MinPrice]);
-      }
+  createMapOptions (maps) {
+    return {
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: false
     }
+  }
 
-    //add place ID, country name and airport name to separate array
+  selectCity (name) {
+    this.setState(
+      {
+        selectedCity: name
+      },
+      this.scrollToMyRef
+    )
+  }
 
-      for(let i = 0; i < places.length; i++){
-        placesIdCountryAirport.push([places[i].PlaceId, places[i].CountryName, places[i].Name]);
-        placesId.push(places[i].PlaceId);
-      }
-    
-    console.log(placesIdCountryAirport);
-
-    if(quotesDestionationIdPrice.length < 20) {
-      
-       //filter Places respone to include only destination ids that correspond to the Quotes destination ids
-
-      let index;
-
-      for(let i = 0; i < quotesDestionationIdPrice.length; i++){
-        index = placesId.indexOf(quotesDestionationIdPrice[i][0]);
-        if(index !== -1){
-        results.push({country: placesIdCountryAirport[index][1], airport: placesIdCountryAirport[index][2], price:quotesDestionationIdPrice[i][1]});
-      }
-    }
-
-    console.log(quotesDestionationIdPrice.length);
-    console.log(results.length);
-
-      this.setState({ quotes: results }, () => console.log(this.state.quotes));
+  render () {
+    let markers = []
+    if (this.props.coordinates && this.props.countriesData) {
+      markers = this.props.countriesData.map(c => {
+        return (
+          <Marker
+            key={c.id}
+            lat={c.lat}
+            lng={c.lng}
+            name={c.name}
+            price={c.minPrice}
+            selectCountry={this.props.selectCountry}
+            selector='country'
+          />
+        )
+      })
     } else {
-      this.getCheapest(quotesDestionationIdPrice, placesId, placesIdCountryAirport);
+      markers = ''
     }
-  }
 
-  getCheapest(quotesDestionationIdPrice,placesId, placesIdCountryAirport) {
-    const results = [];
-    const sorted = quotesDestionationIdPrice.sort(function(a , b) {
-      return a[1] - b[1];
-     });;
-    const cheapest = sorted.slice(0,20);
-    
-     //filter Places respone to include only destination ids that correspond to the Quotes destination ids
+    const { center, zoom } = this.props.coordinates
 
-    let index;
-
-    for(let i = 0; i < cheapest.length; i++){
-    index = placesId.indexOf(quotesDestionationIdPrice[i][0]);
-    if(index !== -1){
-      results.push({country: placesIdCountryAirport[index][1], airport: placesIdCountryAirport[index][2], price: cheapest[i][1]});
-    }
-  }
-  console.log(cheapest.length);
-  console.log(results.length);
-    this.setState({ quotes: results }, () => console.log(this.state.quotes));
-  }
-
-  render() {
-    return(
-     <select id="dropdown"  onChange={this.handleChange}>
-          <option value="Select" defaultValue>Select</option>
-          <option value="ABZ">Aberdeen</option>
-          <option value="BFS">Belfast</option>
-          <option value="BHX">Birmingham</option>
-          <option value="BRS">Bristol</option>
-          <option value="CWL">Cardiff</option>
-          <option value="EDI">Edinburgh</option>
-          <option value="EXT">Exeter</option>
-          <option value="GLA">Glasgow</option>
-          <option value="LBA">Leeds Bradford</option>
-          <option value="LPL">Liverpool</option>
-          <option value="LOND">London</option>
-          <option value="MAN">Manchester</option>
-          <option value="NCL">Newcastle</option>
-          <option value="SOU">Southampton</option>
-      </select>
+    return (
+      <div style={{ width: '80vw', height: '60vh', margin: 'auto' }}>
+        <GoogleMapReact
+          options={this.createMapOptions}
+          bootstrapURLKeys={{ key: 'AIzaSyBgQWwReU8XuTMbmvGgbMBkilrVnJm9dzM' }}
+          center={center}
+          zoom={zoom}
+        >
+          {markers}
+        </GoogleMapReact>
+      </div>
     )
   }
 }

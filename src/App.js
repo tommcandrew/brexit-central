a839f5157e5de899adaa248f923c3b9d710caafd
marco981 <marco98x2@gmail.com>
2019-10-05 18:36:51 +0100

    add title

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import Container from './Components/Container/Container'
 import NewsAggregator from './Components/NewsAggregator/NewsAggregator'
 import FlightFinder from './Components/FlightFinder/FlightFinder'
 import Timeline from './Components/Timeline/Timeline'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-
-    <div className="app-wrapper">
-
+    <div className='app-wrapper'>
       <BrowserRouter>
-        <h1 className="container__title"><Link className="router-link" to="/">Brexit Planner</Link></h1>
+        <Link className='router-link' to='/'>
+          <div className='flags-container'>
+            <span className='flags brit-flag'>Bre</span>
+            <span className='flags euro-flag'>xit</span>
+          </div>
+          Planner
+        </Link>
         <Route exact path='/' component={Container} />
-        <Route path='/news' component={NewsAggregator}></Route>
-        <Route path='/travel' component={FlightFinder}></Route>
-        <Route path='/timeline' component={Timeline}></Route>
-
+        <Route path='/news' component={NewsAggregator} />
+        <Route path='/travel' component={FlightFinder} />
+        <Route path='/timeline' component={Timeline} />
       </BrowserRouter>
-
     </div>
-
   )
 }
 

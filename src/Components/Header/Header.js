636dc816e90d49f83bfca3405ddadc499cd89c1d
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-13 13:14:41 +0100

    make header responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header(props) {
     return (
         <div className="header-container">
-            <span className='router-link' onClick={() => props.updateNextPage('home')}>
+            <span className='home-link' onClick={() => props.updateNextPage('home')}>
                 <div className='flags-container'>
                     <span className='flags brit-flag'>Bre</span>
                     <span className='flags euro-flag'>xit</span>

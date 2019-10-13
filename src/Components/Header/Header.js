375b75b21cd62073b4cfe9d4348842bbdc343660
marco981 <marco98x2@gmail.com>
2019-10-13 20:57:15 +0100

    style changes to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import './Header.css'
 
-export default function Header(props) {
-    return (
-        <div className="header-container">
-            <span className='home-link' onClick={() => props.updateNextPage('home')}>
-                <div className='flags-container'>
-                    <span className='flags brit-flag'>Bre</span>
-                    <span className='flags euro-flag'>xit</span>
-                </div>
-                <span className="planner">
-                    Planner
-                </span>
-            </span>
+export default function Header (props) {
+  return (
+    <div className='header-container'>
+      <span className='home-link' onClick={() => props.updateNextPage('home')}>
+        <div className='flags-container'>
+          <span className='flags brit-flag'>Bre</span>
+          <span className='flags euro-flag'>xit</span>
         </div>
-    )
+        <span className='planner'>Planner</span>
+      </span>
+    </div>
+  )
 }
